991deb5c491d38b97718ff7f8580cb7651e13f86
BareaMartin <martin.barea94@gmail.com>
2022-11-08 20:07:43 -0300

    MCGA-00: changes to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import Login from './screens/Login';
-import Products from './screens/Products';
-import Layout from './components/Layout';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./screens/Login";
+import Layout from "./components/Layout";
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="*" element={<Navigate to={'/login'}  />} />
-      </Routes>
-    </Layout>
+    <>
+      <Layout
+        children={
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            {/* <Route path="/products" element={<Products />} /> */}
+          </Routes>
+        }
+      />
+    </>
   );
 }
 
